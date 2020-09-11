13b1754edfd6e349e6ff25743fa2698173f00ba2
matias <matiaslavalle150396@hotmail.com>
2020-09-11 13:25:00 -0300

    Updates

---
+++ precache-manifest.438321e7c5bf04d5c20bc342f7525ffc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c467cc934ca56696c8ffa207f4be510a",
+    "url": "/proyecto-1/index.html"
+  },
+  {
+    "revision": "f88f4b739ff6b6d2ad86",
+    "url": "/proyecto-1/static/css/main.f6c56b20.chunk.css"
+  },
+  {
+    "revision": "c1995cd3bdc56462d898",
+    "url": "/proyecto-1/static/js/2.306e76ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proyecto-1/static/js/2.306e76ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88f4b739ff6b6d2ad86",
+    "url": "/proyecto-1/static/js/main.8399efb2.chunk.js"
+  },
+  {
+    "revision": "063f1d74e138ae8a52ad",
+    "url": "/proyecto-1/static/js/runtime-main.9ae12b9c.js"
+  },
+  {
+    "revision": "3c7ab2057938199ff148f76892e6be9a",
+    "url": "/proyecto-1/static/media/fondoMosaico.3c7ab205.jpg"
+  }
+]);
